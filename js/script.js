0a09733979469fa6a6b53dc3904ad10cf0d18634
Yushchuk Dmytro <Dreuk03@gmail.com>
2022-05-28 22:59:39 +0300

    Lesson №20

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+"use strict";
+
+// Lesson № 19
+
 const numberOfFilms = +prompt('Скільки фільмів ви вже подивились?', '');
 
 const personalMovieDB = {
@@ -17,3 +21,34 @@ personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
+
+// Lesson №20
+
+// const num = 50;
+
+// if (num < 49) {
+// 	console.log('Error');
+// } else if (num > 100) {
+// 	console.log('too mach...');
+// } else {
+// 	console.log('Nice');
+// }
+
+// // (num === 50) ? console.log('Nice') : console.log('Error');  - ТЕРНАРНИЙ ОПЕРАТОР !!!
+
+const num = 50;
+
+switch (num) {
+	case 49:
+		console.log('Error');
+		break;
+	case 50:
+		console.log('Error');
+		break;
+	case 52:
+		console.log('Nice!');
+		break;
+	default:
+		console.log('Wrong ha ha ha!');
+		break;
+}
