e9aeb8bf532ef3274ba24efee69acf3e4816a8fb
Yushchuk Dmytro <Dreuk03@gmail.com>
2022-06-03 23:02:13 +0300

    Practice(*) № 8.

--- js/script2.js
+++ js/script2.js
@@ -333,3 +333,22 @@ function calculateVolumeAndArea(length) {
 
 
 calculateVolumeAndArea(5);
+
+
+//  ---------------------- Упражненние по написанию кода № 8 ----------------------
+
+
+function findMaxNumber(a, b, c, d) {
+	
+	if(typeof(a) !== "number" ||
+		typeof(b) !== "number" ||
+		typeof(c) !== "number" ||
+		typeof(d) !== "number"
+	) {
+		return 0;
+	} else {
+		return Math.max(a,b,c,d);
+	}
+}
+findMaxNumber(5,6,7,34);
+
