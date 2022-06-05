aa9231d3fd07c1de7488f1c215724ab5dda010a8
Yushchuk Dmytro <Dreuk03@gmail.com>
2022-06-05 21:42:55 +0300

    Lesson №32 Object

--- js/script2.js
+++ js/script2.js
@@ -370,3 +370,29 @@ function nice2() {
 }
 
 andre("Чорт", nice, nice2);
+
+
+//  ---------------------- Lesson № 32 Object ----------------------
+
+const options = {
+	name: "test",
+	width: 1024,
+	height: 1024,
+	colour: {
+		border: "black",
+		bg: "red"
+	}
+};
+
+for (let key in options) {
+if(typeof(options[key]) === "object") {
+	for(let i in options[key]) {
+		console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+	}
+
+	} else {
+		console.log(`Свойство ${key} имеет значение ${options[key]}`);
+	}
+}
+
+
